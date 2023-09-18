d8570ee0570f950538dfd7794664bd48ff82d231
Pis4ni <pis4wrk@gmail.com>
2023-09-18 14:49:23 +0200

    add: milestone 0

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import axios from "axios";
+import { store } from "./data/store";
+
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello vue",
+      store
+    }
+  },
+  methods:{
+    fetchFilmsByQuery (query){
+      store.films = []
+      axios.get("" + query).then((res) => {
+        store.films = res.data.data;
+      },)},
+    fetchTvSeriesByQuery (query){
+      store.series = []
+      axios.get("" + query).then((res) => {
+        store.series = res.data.data;
+      },)},
+    handleSearch(query){
+      fetchFilmsByQuery(query)
+      fetchTvSeriesByQuery(query)
+    }
+
+},
+  components: {
+    AppHeader,AppMain
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import App from './App.vue'
+
+import "../node_modules/flag-icons/css/flag-icons.min.css";
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
