cb96fbdbf2a8fee34c5b7f8fa157ef105f85d4f4
Pis4ni <pis4wrk@gmail.com>
2023-09-18 14:49:56 +0200

    add: milestone 0 missded files

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,18 +4,48 @@
 export default {
   data() {
     return {
-      title: "Hello header"
-    }
-  }
+      title: "boolflix",
+    };
+  },
 
   // components: {
   //   MyComponent,
   // },
 };
 </script>
-
+<!-- todo  devo costruire un header con un logo e una barra di ricerca, l header fara un emit il quale conterra il valore della query cercato dall utente -->
 <template>
-  <h1>{{ title }}</h1>
+  <header class="p-3">
+    <div class="container">
+      <div class="d-flex justify-content-between align-items-center">
+        <h1 class="text-uppercase text-danger">{{ title }}</h1>
+        
+
+        <div class="input-group w-50">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Search for films or TV Series"
+            aria-label="searchBar"
+            aria-describedby="button-searchBar"
+          />
+          <button
+            class="btn btn-outline-danger"
+            type="button"
+            id="button-searchBar"
+          >
+            <!-- todo fa icon -->
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header{
+    background-color: #000000;
+
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,31 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
+import cardList from "../components/cards/cardList.vue";
 
 export default {
   data() {
     return {
       title: "Hello main"
     }
-  }
-
-  // components: {
-  //   MyComponent,
-  // },
+  },
+  components: {
+    cardList,
+  },
 };
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+    <main>
+        <!-- film list -->
+        <cardList />
+        
+        <!-- TV Series list -->
+        <cardList />
+    </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    main{
+        height: calc(100vh - 86px);
+        background-color: #343434;
+    }
+</style>
